5117a23014d2824c8e868748bf078ecd20060bf5
atehio <a.tehio-pesselet@octo.com>
2023-09-14 09:17:52 +0200

    delete tf azure

--- src/infra/azure/datasource.tf
+++
@@ -1,6 +0,0 @@
-data "azurerm_client_config" "current" {
-}
-
-data "azurerm_subscription" "PSAzure" {
-  subscription_id = "8ac3928d-ce93-403b-b8ff-9511282367af"
-}
--- src/infra/azure/rg.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg_alltribes" {
-  name     = "rg-alltribes"
-  location = local.location
-}
