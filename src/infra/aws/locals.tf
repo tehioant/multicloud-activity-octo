f5292831f97dddcce6355dd57f44ffc28f8d1a4c
atehio <a.tehio-pesselet@octo.com>
2023-09-14 09:15:27 +0200

    delete tf aws

--- src/infra/aws/locals.tf
+++ src/infra/aws/locals.tf
@@ -1,6 +1,6 @@
 locals {
   mandatoryTags = {
-    owner = "ante"
+    owner = "alltribes"
   }
   location = "francecentral"
 }
--- src/infra/aws/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "s3" {
-  bucket = "bucket-alltribes"
-
-  tags = {
-    Owner = "ANTE"
-    Name        = "ante-bucket"
-    Environment = "dev"
-  }
-}
